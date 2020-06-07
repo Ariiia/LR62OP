40722275bb8516395fc2eb1e5cda5b89a1c67848
Dariia Kariavka <koalaansthen@gmail.com>
2020-06-07 20:51:32 +0300

    count unpaids

--- Headmisstress.cs
+++ Headmisstress.cs
@@ -6,13 +6,25 @@ namespace LR62OP
 {
  public   class Headmisstress
     {
-        public static int cost=;
+        public static int cost=500;
 
-
+        List<int> Sum = new List<int>();
 
         public static void ReceiptForParents()
         {
-            
+            if (Child.Days < 30)
+                Console.WriteLine("Not enought days to calculate");
+            else
+            {
+                foreach (Child element in Child.MyBoys ) {
+                    int sum = 0;
+
+                    for (int i = 0; i < Child.Days; i++)
+                    {
+                        sum = ((element.Attend[i]) == "+") ? sum += cost : sum; 
+
+                    } }
+            }
 
 
 
