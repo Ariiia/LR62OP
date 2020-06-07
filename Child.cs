d21a1fd8ad841e6ba8d2b81f93dfd979cadb5526
Dariia Kariavka <koalaansthen@gmail.com>
2020-06-07 13:19:06 +0300

    at least I have days and children

--- Child.cs
+++ Child.cs
@@ -6,26 +6,37 @@ namespace LR62OP
 {
     class Child
     {
-        
-     public int[] DayNumber;
-    public    string[] Attend;
+
+        public int[] DayNumber;
+        public string[] Attend;
 
 
-     public   Child( int days)
+        public Child(int days)
         {
             this.DayNumber = new int[days];
             this.Attend = new string[days];
             string[] ChooseFrom = { "+", "-" };
-            string AttendBool;
+            int AttendBool;
             Random rand = new Random();
-            AttendBool = Convert.ToString(rand.Next(ChooseFrom.Length));
-               
-           
-            for (int i=0; i<days; i++)
-            {
-                this.DayNumber = i + 1;
 
+
+            for (int i = 0; i < days; i++)
+            {
+                AttendBool = rand.Next(ChooseFrom.Length);
+
+                this.DayNumber[i] = i + 1;
+                this.Attend[i] = ChooseFrom[AttendBool];
             }
         }
+
+        public void Output(int days)
+        {
+            for (int i = 0; i < days; i++)
+            {
+                Console.WriteLine("DAY " + DayNumber[i] + ":\t\t"+Attend[i]);
+            }
+
+        }
+
     }
 }
--- Program.cs
+++ Program.cs
@@ -6,21 +6,29 @@ namespace LR62OP
     {
         static void Main(string[] args)
         {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
             Console.WriteLine("Отобразить информацию о детях, которым необходимо принести медицинскую справку.");
             Console.WriteLine("Kariavka Dariia, 10 variant, level B(C)");
             Console.WriteLine("Please Enter number of children");
-            int days = 30;//number of days in kindergarden
-            int ChildSize = 1;// Convert.ToInt32(Console.ReadLine());
+            int days = 15;//number of days in kindergarden
+            int ChildSize = 3;// Convert.ToInt32(Console.ReadLine());
             Child[] children = new Child[ChildSize];//array of children
-            int[] Daynumber = new int[days];//array of daynumbers
-            Random rand = new Random();
-            string[] ChooseFrom = { "+", "-"};
-            string AttendBool;
+
             for (int i = 0; i < ChildSize; i++)
-            { for (int j = 0; j < days; j++) {
-                    AttendBool = Convert.ToString(rand.Next(ChooseFrom.Length));
-                    children[i] = new Child(30, j, AttendBool);
-             } }
+            { 
+                    children[i] = new Child(days);
+             }
+
+            //OUTPUT
+            for (int i = 0; i < ChildSize; i++)
+            {
+                Console.WriteLine("\nChild #\t" + (i + 1)+":\n");
+                children[i].Output(days);
+              
+            }
+
+
         }
     }
 }
