825d360180917891c43d5f05a0091dbf967000b5
Dariia Kariavka <koalaansthen@gmail.com>
2020-06-07 12:43:07 +0300

    fix child array

--- Child.cs
+++ Child.cs
@@ -6,5 +6,19 @@ namespace LR62OP
 {
     class Child
     {
+        int days;
+     public int[] DayNumber;
+    public    string Attend;
+
+
+     public   Child(int days, int[] DayNumber,  string Attend)
+        {
+            for(int i=0; i<days; i++)
+            {
+                DayNumber[i] = i + 1;
+
+            }
+                this.Attend = Attend;
+        }
     }
 }
